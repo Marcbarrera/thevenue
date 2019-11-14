86fd5d97be10ecc546eaa65e88a2a8223ee48170
marcbarrera <barreramarinmarc@gmail.com>
2019-11-14 09:29:54 +0100

    slider done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './resources/styles.css';
+
 import Header from'./components/header_footer/Header';
+import Featured from './components/featured';
 
 
 function App() {
   return (
     <div className="App" style= {{height:"1500px",background:'cornflowerblue'}}>
       <Header/>
+      <Featured/>
     </div>
   );
 }
---
+++ src/components/featured/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Carrousel from './Carrousel'; 
+
+const Featured = () => {
+    return (
+        <div style={{position:'relative'}}>
+
+            <Carrousel/>
+
+            <div className="artist_name">
+                <div className="wrapper">
+                    Ariana Grande
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
